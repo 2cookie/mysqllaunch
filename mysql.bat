4bbfccf050930f30699a2949c813007e4850ef01
2cookie <qaandrain@outlook.com>
2025-05-21 02:50:07 -0600

    Add files via upload

---
+++ mysql.bat
@@ -0,0 +1,4 @@
+echo Starting mysql windows! (please make sure to run as admin)
+echo Created with love by 2cookie <3 
+cd C:\Program Files\MySQL\MySQL Server 8.0
+mysqld --defaults-file="C:\ProgramData\MySQL\MySQL Server 8.0\my.ini"
